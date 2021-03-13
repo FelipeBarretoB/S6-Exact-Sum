92da4ff09d5a596343c68bda3bdfd32b37609343
unknown <pipe.barreto07@gamil.com>
2021-03-12 23:51:52 -0500

    main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -26,45 +26,49 @@ public class Main {
 		int [] intBook;
 		int M=0;
 		ArrayList<String> print=new ArrayList<>();
+		
 		try {
 			String first= ui.br.readLine();
-			while(first !=null){  
-				N= Integer.parseInt(first);
-				book=ui.br.readLine();
-				bookArray=book.split(" ");
-				intBook= new int[N];
-				for(int c=0; c<N;c++) {
-					intBook[c]=Integer.parseInt(bookArray[c]);
-				}
-				M= Integer.parseInt(ui.br.readLine());
-				int i=0;
-				int j=0;
-		
-				Arrays.sort(intBook);
+			while(!first.equals("")){
 				
-				
-				for(int c=0; c<intBook.length;c++) {
-					int x=intBook[c];
-					int y=M-x;
-					for( int z=0;z<N;z++) {
-						if(intBook[z]==y && z!=c) {
-							if(i==0 && j==0 ) {
-								i=x;
-								j=y;
-							}else if(Math.abs(i-j)>Math.abs(x-y)){
-								i=x;
-								j=y;
+					N= Integer.parseInt(first);
+					book=ui.br.readLine();
+					bookArray=book.split(" ");
+					intBook= new int[N];
+					for(int c=0; c<N;c++) {
+						intBook[c]=Integer.parseInt(bookArray[c]);
+					}
+					M= Integer.parseInt(ui.br.readLine());
+					int i=0;
+					int j=0;
+
+					Arrays.sort(intBook);
+
+
+					for(int c=0; c<intBook.length;c++) {
+						int x=intBook[c];
+						int y=M-x;
+						for( int z=0;z<N;z++) {
+							if(intBook[z]==y && z!=c) {
+								if(i==0 && j==0 ) {
+									i=x;
+									j=y;
+								}else if(Math.abs(i-j)>Math.abs(x-y)){
+									i=x;
+									j=y;
+								}
 							}
 						}
 					}
+
+					print.add("Peter should buy books whose prices are "+intBook[Arrays.binarySearch(intBook,i)]+" and "+intBook[Arrays.binarySearch(intBook,j)]+".");
+					ui.br.readLine();
+					first= ui.br.readLine();
 				}
-				ui.bw.write("\n");
-				ui.bw.flush();
-				print.add("Peter should buy books whose prices are "+intBook[Arrays.binarySearch(intBook,i)]+" and "+intBook[Arrays.binarySearch(intBook,j)]+".");
-				first= ui.br.readLine();
-			}
+			
 			for(int c=0;c <print.size();c++) {
-				ui.bw.write(print.get(c)+"\n \n");
+				ui.bw.write(print.get(c)+"\n");
+				ui.bw.newLine();
 				ui.bw.flush();
 			}
 		} catch (IOException e) {
